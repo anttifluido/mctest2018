0fa92bd8dbb80be0f1c4bf5003e9c136dd240966
Antti Paukku <anttipaukku@Anttis-MacBook-Pro-2.local>
2018-04-23 10:53:26 +0200

    fuel test

--- index.js
+++ index.js
@@ -46,6 +46,7 @@ express()
     );
 
     var reqoptions = {
+      CustomerKey: 'B1888E05-F9DE-42E1-A96D-3DC720957583',
       filter: {
         leftOperand: 'Client',
         operator: 'equals',
@@ -54,7 +55,7 @@ express()
     };
 
     SoapClient.retrieve(
-      'DataExtensionObject["B1888E05-F9DE-42E1-A96D-3DC720957583"]',
+      'DataExtensionObject',
       ["ID","Properties"],
       reqoptions,
       function( err, response ) {
