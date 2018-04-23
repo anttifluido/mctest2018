75c7205e9be5243bd9aaca6ed050f72f9e618e3d
Antti Paukku <anttipaukku@Anttis-MacBook-Pro-2.local>
2018-04-23 09:18:14 +0200

    edit page

--- index.js
+++ index.js
@@ -7,4 +7,8 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/edit', (req, res) => res.render('pages/edit'))
+  .get('/save', function(request, responsefromWeb) {
+    console.log(request);
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
