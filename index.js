1b072e63053f50cc075cd62eff84005bca6fb6d9
Antti Paukku <anttipaukku@Anttis-MacBook-Pro-2.local>
2018-04-23 09:56:23 +0200

    fuel test

--- index.js
+++ index.js
@@ -8,7 +8,42 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/edit', (req, res) => {
-    
+    var FuelSoap = require('fuel-soap');
+    var options = {
+      auth: {
+        clientId: process.env.CLIENT_ID
+        , clientSecret: process.env.CLIENT_SECRET
+      }
+      , soapEndpoint: 'https://webservice.s6.exacttarget.com/Service.asmx' // default --> https://webservice.exacttarget.com/Service.asmx
+    };
+
+    var SoapClient = new FuelSoap(options);
+
+    var options = {
+      filter: {
+        leftOperand: 'CustomerKey',
+        operator: 'equals',
+        rightOperand: '05D89268-E818-4B4E-B4DA-56EB549CA3FB'
+      }
+    };
+
+    SoapClient.retrieve(
+      'DataExtension',
+      ["Description"],
+      options,
+      function( err, response ) {
+        if ( err ) {
+          // error here
+          console.log( err );
+          return;
+        }
+
+        // response.body === parsed soap response (JSON)
+        // response.res === full response from request client
+        console.log( response.body );
+      }
+    );
+
     res.render('pages/edit');
   })
   .get('/save', function(request, response) {
