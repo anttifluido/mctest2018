e2cfd7b5e9e24089856e768eac1247b26cc35858
Antti Paukku <anttipaukku@Anttis-MacBook-Pro-2.local>
2018-04-23 11:25:28 +0200

    fuel test

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ express()
 
     SoapClient.retrieve(
       'DataExtensionObject[EmailContent]',
-      ["Content"],
+      ["Client","Content","Modified"],
       reqoptions,
       function( err, response ) {
         if ( err ) {
