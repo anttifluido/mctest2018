c29712242c8796e84b4243f3b9328836986ea98b
Antti Paukku <anttipaukku@Anttis-MacBook-Pro-2.local>
2018-04-27 19:48:15 +0300

    new structure

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 var loginContoller = require('./controller/login');
+const bodyParser = require('body-parser');
 
 
 const app = express()
+app.use(bodyParser);
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
