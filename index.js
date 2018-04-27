93cb1a7edd5ef5d00d4004955248f977f6648a2f
Antti Paukku <anttipaukku@Anttis-MacBook-Pro-2.local>
2018-04-27 19:53:28 +0300

    new structure

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const bodyParser = require('body-parser');
 
 
 const app = express()
-app.use(bodyParser);
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
