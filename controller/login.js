36028fcda06942bfeb6ba710c244d14b61380d83
Antti Paukku <anttipaukku@Anttis-MacBook-Pro-2.local>
2018-04-27 19:55:21 +0300

    new structure

--- controller/login.js
+++ controller/login.js
@@ -4,12 +4,8 @@ module.exports = {
 
     login : function(req, res){
       var jwt = require('jwt-simple');
-      console.log('login');
-      console.log(req);
-      console.log('headers');
-      console.log(req.headers)
-      console.log('body');
-      console.log(req.body)
+
+      console.log(req.bod.jwt)
 
       /*var secret = process.env.SECRET;
        var decodedJWT = jwt.decode(req.query.encodedJWT, secret);
